dae737a65321e382793f8172a8a5ba7e4d3c8cca
Matthew Michihara <michihara@google.com>
2021-10-21 16:44:43 -0400

    test forms

---
+++ src/main/java/com/google/BarForm.java
@@ -0,0 +1,7 @@
+package com.google;
+
+import javax.swing.*;
+
+public class BarForm {
+    private JPanel rootPanel;
+}
--- src/main/java/com/google/FooForm.java
+++ src/main/java/com/google/FooForm.java
@@ -5,4 +5,10 @@ import javax.swing.*;
 public class FooForm extends JPanel {
     private JLabel label;
     JPanel rootPanel;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        label = new JLabel("this is a new label");
+//        rootPanel.add(label2, 0);
+    }
 }
